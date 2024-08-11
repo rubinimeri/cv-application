44f6532c15c348103ca0618ca4e9aaa3bffe077c
Rubin Imeri <rubinimeri6@gmail.com>
2024-08-11 19:07:58 +0200

    Export PersonalDetails to Resume

--- src/components/PersonalDetails/PersonalDetails.jsx
+++ src/components/PersonalDetails/PersonalDetails.jsx
@@ -1,5 +1,23 @@
 function PersonalDetails({ name, jobTitle, number, email, location }) {
     return(
-        
+        <div className="personal-details">
+            <div>
+                <h1>{name}</h1>
+                <p>{jobTitle}</p>
+            </div>
+            <div>
+                <p>
+                    {number}
+                </p>
+                <p>
+                    {email}
+                </p>
+                <p>
+                    {location}
+                </p>
+            </div>
+        </div>
     )
 }
+
+export default PersonalDetails
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,19 @@
+import PersonalDetails from "./PersonalDetails/PersonalDetails"
+
 function Resume({ 
     personalDetails, summary, educationArray, 
     workArray, skills, certifications }) {
-    
+
+    return(
+        <div className="resume">
+            <PersonalDetails
+             name={personalDetails.name}
+             jobTitle={personalDetails.jobTitle}
+             number={personalDetails.number}
+             email={personalDetails.email}
+             location={personalDetails.location} />
+        </div>
+    )
 }
 
 export default Resume

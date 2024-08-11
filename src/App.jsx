4990beb2fbef9931938fa1b53f178527a5615202
Rubin Imeri <rubinimeri6@gmail.com>
2024-08-11 18:53:34 +0200

    Add state variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import exampleData from "./exampleData"
 
 import './styles/App.css'
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState(exampleData.personalInfo);
+  const [summary, setSummary] = useState(exampleData.summary);
+  const [educationalExperience, setEducationalExperience] = useState(exampleData.educationalExperience);
+  const [professionalExperience, setProfessionalExperience] = useState(exampleData.professionalExperience);
+  const [skills, setSkills] = useState(exampleData.skills);
+  const [certifications, setCertifications] = useState(exampleData.certifications);
+
 
   return (
-    <main className="app"></main>
+    <main className="app">
+
+    </main>
+    
   )
 }
 

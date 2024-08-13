eeac9458b36328fb12b96a42f9eeaebb5e4344c4
Rubin Imeri <rubinimeri6@gmail.com>
2024-08-13 17:38:24 +0200

    Define props outside return statement

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,46 @@ function App() {
   const [skills, setSkills] = useState(exampleData.skills);
   const [certifications, setCertifications] = useState(exampleData.certifications);
 
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  function toggleActiveIndex(index) {
+    activeIndex === index ? setActiveIndex(0) : setActiveIndex(index)
+  }
+
+  const summaryProps = {
+    summary: summary,
+    setSummary: setSummary,
+    isShown: activeIndex === 1,
+    toggleShow: toggleActiveIndex
+  }
+
+  const educationProps = {
+    educationArray: educationalExperience,
+    setEducationArray: setEducationalExperience,
+    isShown: activeIndex === 2,
+    toggleShow: toggleActiveIndex
+  }
+
+  const workProps = {
+    workExperience: professionalExperience,
+    setWorkExperience: setProfessionalExperience,
+    isShown: activeIndex === 3,
+    toggleShow: toggleActiveIndex
+  }
+
+  const skillProps = {
+    skills,
+    setSkills,
+    isShown: activeIndex === 4,
+    toggleShow: toggleActiveIndex
+  }
+
+  const certificationProps = {
+    certifications,
+    setCertifications,
+    isShown: activeIndex === 5,
+    toggleShow: toggleActiveIndex
+  }
 
   return (
     <main className="app">
@@ -25,21 +65,11 @@ function App() {
         <PersonalDetailsForm 
          personalInfo={personalInfo}
          setPersonalInfo={setPersonalInfo} />
-        <SummaryForm
-         summary={summary}
-         setSummary={setSummary} />
-        <EducationForm 
-         educationArray={educationalExperience}
-         setEducationArray={setEducationalExperience} />
-        <WorkForm 
-         workExperience={professionalExperience}
-         setWorkExperience={setProfessionalExperience} />
-        <SkillsForm 
-         skills={skills} 
-         setSkills={setSkills} />
-        <CertificationsForm
-         certifications={certifications}
-         setCertifications={setCertifications} />
+        <SummaryForm {...summaryProps} />
+        <EducationForm {...educationProps} />
+        <WorkForm {...workProps} />
+        <SkillsForm {...skillProps} />
+        <CertificationsForm {...certificationProps} />
       </div>
       <Resume 
       personalDetails={personalInfo}
